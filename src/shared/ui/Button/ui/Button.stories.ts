e7e94b2dd2a106e9ba5e6dcae2744cc85056c62e
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2024-01-31 20:40:00 +0300

    change stories

--- src/shared/ui/Button/ui/Button.stories.ts
+++ src/shared/ui/Button/ui/Button.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import '../../../styles/index.scss';
-
 import { Button } from './Button';
+import { THEMES } from '../../../const/themes';
 
 const meta = {
     title: 'shared/Button',
@@ -17,10 +17,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const English: Story = {
+export const ThemeLight: Story = {
     args: { children: 'Button', color: 'normal' },
 };
 
-export const Russian: Story = {
-    args: { children: 'Кнопка', color: 'normal' },
+export const ThemeDark: Story = {
+    args: { children: 'Кнопка', variant: 'outline', color: 'success', className: THEMES.DARK },
 };
