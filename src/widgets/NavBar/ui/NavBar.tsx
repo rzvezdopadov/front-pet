7d9657657d42bf3c5bdb45f54425b2d0ea7813a7
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2024-01-31 20:40:31 +0300

    delete test button

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,12 +1,5 @@
-import { Button } from '@/shared/ui/Button';
 import cls from './NavBar.module.scss';
 
 export function NavBar() {
-    return (
-        <header className={cls.NavBar}>
-            <Button size="m" color="success" variant="outline">
-                Ghbdtn
-            </Button>
-        </header>
-    );
+    return <header className={cls.NavBar}></header>;
 }
